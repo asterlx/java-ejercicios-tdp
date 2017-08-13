d662609c9f4303f10d730ef498755311c1d8b552
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 20:59:00 -0300

    ejercicio 20 branch

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Ejercicios sobre tecnicas de programación (TDP) en Java
 
-Los ejercicios estan separados en diferentes ramas del repositorio, la idea es que cada estudiante descargue el repositorio, elija uno de los ejercicios y lo resuelva.
+## EJERCICIO 20: Programa java que realice lo siguiente
+
+* Calcula el número de la suerte de una persona a partir de su fecha de nacimiento.
+
+## SPOILER ALERT
+
+Posible solución [aquí](http://puntocomnoesunlenguaje.blogspot.com.es/2012/11/java-ejercicios-basicos-resueltos-5.htmlx).
--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,6 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 20: Programa que calcula el número de la suerte de una persona a
+ * partir de su fecha de nacimiento.
  */
 package cac.java.tdp;
 
@@ -15,7 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
