bd87e191ce7a9500926c075ea9288f3966b4e63e
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 21:37:43 -0300

    ejercicio 25

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 25: Programa que lea dos caracteres y compruebe si son dos letras minúsculas.
  */
 package cac.java.tdp;
 
@@ -15,7 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
