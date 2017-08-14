5b0e216d1a72b852fb668e296b07be267836a2df
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 21:46:15 -0300

    ejercicio 28

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 28: Calcular el mayor de tres números enteros en Java.
  */
 package cac.java.tdp;
 
@@ -15,7 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
