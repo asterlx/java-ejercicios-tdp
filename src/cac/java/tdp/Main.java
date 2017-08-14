c8d94f40f99fc82432016926690882cb79ac47f0
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 21:47:49 -0300

    ejercicio 29

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,7 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 29: Programa que lea tres números enteros H, M, S que contienen
+ * hora, minutos y segundos respectivamente, y comprueba si la hora
+ * que indican es una hora válida.
  */
 package cac.java.tdp;
 
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
