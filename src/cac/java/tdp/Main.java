68ce583caf553c66d80e2197c03613ae87034eb1
Rhaynel Parra Aguiar <me@rhynl.io>
2017-08-13 21:34:29 -0300

    ejercicio 24 branch

--- src/cac/java/tdp/Main.java
+++ src/cac/java/tdp/Main.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Ejercicio 24: Programa que lea dos caracteres por teclado y compruebe si son iguales.
  */
 package cac.java.tdp;
 
@@ -15,7 +13,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola Mundo!!");
+        
     }
     
 }
